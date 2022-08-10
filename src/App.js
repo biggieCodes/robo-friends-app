1f219fba17d19d317774656f2fa43e064e7833e8
Abdulsamad Rabo <raboabdulsamad04@gmail.com>
2022-08-10 12:59:25 +0100

    done solving the push bug

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import SearchBox from './SearchBox';
+import CardList from './CardList';
+import users from './users.json';
+import "./App.css";
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: ''
+    }
+  }
+
+  updateSearchInput = (event) => {
+    console.log(event.target.value);
+    this.setState({ searchInput: event.target.value })
+  }
+
+
+
+  render() {
+
+    const filteredRobots = users.filter(item => (
+      item.name.toLowerCase().includes(this.state.searchInput.toLowerCase())
+    ))
+
+    console.log("Filtered Robots: ", filteredRobots);
+
+    return (
+      <div className="">
+        <h1 style={{ textAlign: 'center' }}>Robofriends</h1>
+        <SearchBox updateSearchInput={this.updateSearchInput}/>
+        <CardList clients={filteredRobots}/>
+      </div>
+    );
+  }
+}
+
+export default App;
